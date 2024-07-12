d3df25c4c4dc4891e72d76853ea206af89b8748f
solesen1992 <saramundal@live.dk>
2024-07-12 14:02:39 +0200

    Add project files.

---
+++ Calculator_WPF_application/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Calculator_WPF_application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
